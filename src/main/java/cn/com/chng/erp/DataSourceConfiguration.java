4bf5bd659485f1c79c46ae303ee5a4bbcb561b30
liuyandong <554030404@qq.com>
2018-01-01 20:06:16 +0800

    初始化项目！！！

---
+++ src/main/java/cn/com/chng/erp/DataSourceConfiguration.java
@@ -0,0 +1,20 @@
+package cn.com.chng.erp;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+    @Bean(name = "primaryDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "datasource.primary")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().type(DruidDataSource.class).build();
+    }
+}
